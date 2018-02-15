dc70227c937e427f3400f2809d391e5b9ab18c21
Kenza Boulisfane <kenza.boulisfane@gmail.com>
2018-02-15 11:14:26 -0500

    Database update

--- database/database_files.sql
+++ database/database_files.sql
@@ -1,87 +1,44 @@
-/*User table
- */
 
 
-
-CREATE TABLE User
+create table Question
 (
-  user_ID  INT(10) AUTO_INCREMENT,
-  password VARCHAR(20) NOT NULL,
-  email    VARCHAR(50) NOT NULL,
-  username VARCHAR(10) NOT NULL,
-  PRIMARY KEY (user_ID, password),
-  CONSTRAINT user_ID_UNIQUE
-  UNIQUE (user_ID),
-  CONSTRAINT email_UNIQUE
-  UNIQUE (email),
-  CONSTRAINT username_UNIQUE
-  UNIQUE (username)
+	question_ID int(10) not null
+		primary key,
+	question varchar(250) not null,
+	answer_ID int(10) not null,
+	answer varchar(250) not null,
+	category varchar(45) not null,
+	user_ID int(10) not null,
+	constraint question_ID_UNIQUE
+		unique (question_ID),
+	constraint answer_ID_UNIQUE
+		unique (answer_ID),
+	constraint user_ID_UNIQUE
+		unique (user_ID)
 )
-  ENGINE = InnoDB;
+engine=InnoDB
+;
 
-/*Question table
- */
-
-CREATE TABLE Question
+create table User
 (
-  question_ID INT(10) AUTO_INCREMENT
-    PRIMARY KEY,
-  user_ID     INT(10)      NOT NULL,
-  question    VARCHAR(250) NOT NULL,
-  categoryID  INT(10)      NOT NULL,
-  CONSTRAINT question_ID_UNIQUE
-  UNIQUE (question_ID),
-  CONSTRAINT user_ID_UNIQUE
-  UNIQUE (user_ID),
-  CONSTRAINT user_ID
-  FOREIGN KEY (user_ID) REFERENCES User (user_ID),
-  CONSTRAINT category_ID
-  FOREIGN KEY (categoryID) REFERENCES Category (category_ID)
+	user_ID int(10) auto_increment
+		primary key,
+	username varchar(25) not null,
+	email varchar(100) not null,
+	password varchar(25) not null,
+	create_time timestamp default CURRENT_TIMESTAMP not null,
+	is_Solver tinyint(1) not null,
+	constraint user_ID_UNIQUE
+		unique (user_ID),
+	constraint username_UNIQUE
+		unique (username),
+	constraint email_UNIQUE
+		unique (email)
 )
-  ENGINE = InnoDB;
+engine=InnoDB
+;
 
-CREATE INDEX user_ID_idx
-  ON Question (user_ID);
-
-CREATE INDEX category_ID_idx
-  ON Question (categoryID);
-
-/*Answer table
- */
-
-CREATE TABLE Answer
-(
-  answer_ID  INT(10) AUTO_INCREMENT
-    PRIMARY KEY,
-  userID     INT(10) NOT NULL,
-  questionID INT(10) NOT NULL,
-  categoryID INT(10) NOT NULL,
-  solver     TINYINT NULL,
-  CONSTRAINT answer_ID_UNIQUE
-  UNIQUE (answer_ID),
-  CONSTRAINT userID_UNIQUE
-  UNIQUE (userID),
-  CONSTRAINT question_ID_UNIQUE
-  UNIQUE (questionID),
-  CONSTRAINT categoryID_UNIQUE
-  UNIQUE (categoryID),
-  CONSTRAINT userID
-  FOREIGN KEY (userID) REFERENCES User (user_ID),
-  CONSTRAINT questionID
-  FOREIGN KEY (questionID) REFERENCES Question (question_ID),
-  CONSTRAINT categoryID
-  FOREIGN KEY (categoryID) REFERENCES Category (category_ID)
-)
-  ENGINE = InnoDB;
-
-/*Category table
- */
-
-CREATE TABLE Category
-(
-  category_ID   INT(10) AUTO_INCREMENT,
-  category_name VARCHAR(20) NOT NULL,
-  CONSTRAINT category_ID_UNIQUE
-  UNIQUE (category_ID)
-)
-  ENGINE = InnoDB;
+alter table Question
+	add constraint user_ID
+		foreign key (user_ID) references User (user_ID)
+;
