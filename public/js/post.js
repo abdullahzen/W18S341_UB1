521f2f69fdce642e249f82a0414728bb00fddad1
Kenza Boulisfane <kenza.boulisfane@gmail.com>
2018-02-15 23:27:06 -0500

    post js

--- public/js/post.js
+++ public/js/post.js
@@ -1,24 +1,18 @@
-
-
 $(function(){
-    $("#postButton").click(function (e) {
-        e.preventDefault();
+    $("#newpostButton").click(function () {
         /*$(". ").hide();*/
-        var request = $.get('/post', $('#postForm').serialize());
+        var request = $.get('./post', $('#postForm').serialize());
         request.done(function () {
             window.location = '/';
         });
 
-        alert('I failed');
         request.fail(function (jqXHR) {
             var data = $.parseJSON(jqXHR.responseText);
             var err = data.message;
 
             var errMsg = '<div class=\"col-md-12 showErr\">' + err + '</div>';
 
-            $("#loginError").empty().append(errMsg);
+            $("#postError").empty().append(errMsg);
         });
     });
 });
-
-
