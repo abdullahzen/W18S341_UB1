26b557e2566237788be73634cce72875f33b1bf3
Kenza Boulisfane <kenza.boulisfane@gmail.com>
2018-02-15 22:40:37 -0500

    post js

--- public/js/post.js
+++ public/js/post.js
@@ -9,13 +9,16 @@ $(function(){
             window.location = '/';
         });
 
+        alert('I failed');
         request.fail(function (jqXHR) {
             var data = $.parseJSON(jqXHR.responseText);
             var err = data.message;
 
             var errMsg = '<div class=\"col-md-12 showErr\">' + err + '</div>';
 
-            /*$("#loginError").empty().append(errMsg);*/
+            $("#loginError").empty().append(errMsg);
         });
     });
 });
+
+
