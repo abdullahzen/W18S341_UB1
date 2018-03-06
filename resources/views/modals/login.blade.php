f4774a4f450e53d8f3d66f890423544e547e2a8d
Hicham Daou <Hicham_Daou@hotmail.com>
2018-03-05 22:04:20 -0500

    login page
    undid comment

--- resources/views/modals/login.blade.php
+++ resources/views/modals/login.blade.php
@@ -91,15 +91,11 @@ button:hover {
         </form>
     </div>
 </div>
-{{--  
 <link class="user" href="{{URL::asset('css/salesforce-lightning-design-system-vf.min.css')}}" rel="stylesheet" type="text/css">
 <link class="user" href="{{URL::asset('css/mainpage.css')}}" rel="stylesheet" type="text/css">
 <link class="user" href="{{URL::asset('css/ourstyle.css')}}" rel="stylesheet" type="text/css">
 <link rel="icon" type="image/x-icon">
 <script src="{{URL::asset('https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js')}}"></script>
 <script src="{{URL::asset('js/mainpage.js')}}"></script>
-<script src="{{ URL::asset('js/login.js') }}" type="text/javascript"></script>  --}}
-
-
-
+<script src="{{ URL::asset('js/login.js') }}" type="text/javascript"></script>
 </html>
