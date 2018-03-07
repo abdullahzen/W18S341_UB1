8465c468a92cab799e3dd6a0a5ac20bcfc102d6a
Nour El Natour <noor_elnatour@hotmail.com>
2018-03-07 11:17:07 -0500

    Added js for all the edit question functions, and edited the modal

--- public/js/editQuestion.js
+++ public/js/editQuestion.js
@@ -1,9 +1,11 @@
 $(function(){
-    $("#saveButton").click(function () {
+    $("#editButton").click(function () {
         $(". ").hide();
-        var request = $.post('./editQuestion/do', $('#editForm').serialize());
+        var id = document.getElementById('hiddenID').value;
+        var request = $.get('/edit', $('#editForm').serialize());
         request.done(function () {
-            window.location = '/';
+            window.location = './post/' + id;
+
         });
 
         request.fail(function (jqXHR) {
@@ -16,3 +18,42 @@ $(function(){
         });
     });
 });
+
+var spanE = document.getElementsByClassName("closeE")[0];
+
+var editModal = document.getElementById('editModal');
+var editQuestion = document.getElementById('editQuestionButton');
+
+spanE.onclick = function() {
+        editModal.style.display = "none";
+    }
+
+if(editQuestion != null){
+    editQuestion.onclick = function() {
+        editModal.style.display = "block";
+        getCategory(document.getElementById('hiddenCategory').value);
+    }
+}
+
+// Fix this if you want
+window.onclick = function(event) {
+    if (event.target == editModal) {
+        editModal.style.display = "none";
+    }
+}
+
+function getCategory(category){
+    document.getElementById(category + 'e').selected = true;
+}
+
+$("#More-Options").click(function () {
+    if ($('#More-Options-Container').css('display') == 'none') {
+        $("#More-Options-Container").css('display' , 'block');
+    }
+    else {
+        $("#More-Options-Container").css('display' , 'none');
+    }
+});
+
+
+
--- resources/views/modals/edit.blade.php
+++ resources/views/modals/edit.blade.php
@@ -1,23 +1,24 @@
 <div id="editModal" class="modal">
     <div class="modal-content">
-        <form action="./edit" id="editForm" method="post">
+        <form action="/edit" id="editForm" method="get">
             {{ csrf_field() }}
             <div class="slds-scope">
                 <div class="demo-only" style="height: 640px;">
                     <section aria-describedby="modal-content-id-1" aria-labelledby="modal-heading-01" aria-modal="true" class="slds-modal slds-fade-in-open" role="dialog" tabindex="-1">
                         <div class="slds-modal__container">
                             <header class="slds-modal__header">
-                                <span class="closeR">&times;</span>
+                                <span class="closeE">X</span>
                                 <h2 class="slds-text-heading_medium slds-hyphenate" id="modal-heading-01">Edit Details</h2>
                             </header>
                             <div class="modal-body">
                                 <div class="slds-form-element">
                                     <label class="slds-form-element__label" for="textarea-id-01">Language</label>
-                                    <select name="category" id>
-                                        <option class="form-control" value="Java">Java</option>
-                                        <option class="form-control" value="JavaScript">JavaScript</option>
-                                        <option class="form-control" value="Php">Php</option>
-                                        <option class="form-control" value="C">C</option>
+                                    <select name="category">
+                                        <option class="form-control" value="Php" id="Phpe">Php</option>
+                                        <option class="form-control" value="C" id="Ce">C</option>
+                                        <option class="form-control" value="Java" id="Javae">Java</option>
+                                        <option class="form-control" value="JavaScript" id="JavaScripte">JavaScript</option>
+
                                     </select>
                                 </div>
 
@@ -25,30 +26,23 @@
                                 <div class="slds-form-element">
                                     <label class="slds-form-element__label" for="textarea-id-01">Title</label>
                                     <div class="slds-form-element__control">
-                                        <input type="text" class="form-control input-md" name="title" id="title"/>{{\App\Http\Controllers\ClientControllerHelper::getQuestionDataFromDBForCurrentUser('title')}}
+
+                                        <input type="text" class="form-control input-md" name="title" id="title" value="{{\App\Http\Controllers\ClientControllerHelper::getQuestionDataFromDBForCurrentQuestion('title', \App\Http\Controllers\ClientControllerHelper::$currentQuestionID)}}"/>
                                     </div>
                                 </div>
                                 <!-- Textarea -->
                                 <div class="slds-form-element">
                                     <label class="slds-form-element__label" for="textarea-id-01">Question</label>
                                     <div class="slds-form-element__control">
-                                        <input type="text" class="form-control input-md" style="width:50%" class="slds-textarea" name="content" id="content"/>{{\App\Http\Controllers\ClientControllerHelper::getQuestionDataFromDBForCurrentUser('content')}}
+                                        <input type="text" class="form-control input-md" style="width:50%" class="slds-textarea" name="content" id="editContent" value="{{\App\Http\Controllers\ClientControllerHelper::getQuestionDataFromDBForCurrentQuestion('content', \App\Http\Controllers\ClientControllerHelper::$currentQuestionID)}}"/>
                                     </div>
                                 </div>
+                                <input type="text" style="visibility: hidden;" name="hiddenID" id="hiddenID" value="{{\App\Http\Controllers\ClientControllerHelper::$currentQuestionID}}"/>
+                                <input type="text" style="visibility: hidden;" id="hiddenCategory" value="{{\App\Http\Controllers\ClientControllerHelper::getCategoryName(\App\Http\Controllers\ClientControllerHelper::$currentQuestionID)}}"/>
+
                                 <div class="form-group">
-                                    <input type="submit" id="saveButton" class="btn btn-block btn-md btn-primary" value="Save"/>
+                                    <input type="submit" id="editButton" class="btn btn-block btn-md btn-primary" value="Save"/>
                                 </div>
-                                {{--<fieldset class="slds-form-element">
-                                    <div class="slds-form-element__control">
-                                      <span class="slds-checkbox">
-                                      <input id="checkbox-48" name="default" type="checkbox" value="on">
-                                      <label class="slds-checkbox__label" for="checkbox-48">
-                                      <span class="slds-checkbox_faux"></span>
-                                      <span class="slds-form-element__label">I agree to terms and conditions</span>
-                                      </label>
-                                      </span>
-                                    </div>
-                                </fieldset>--}}
                             </div>
                             <footer class="slds-modal__footer">
                                 {{--<button class="slds-button slds-button_brand" id="loginButton">Log in</button>--}}
@@ -66,5 +60,4 @@
 <link class="user" href="{{URL::asset('css/ourstyle.css')}}" rel="stylesheet" type="text/css">
 <link rel="icon" type="image/x-icon">
 <script src="{{URL::asset('https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js')}}"></script>
-<script src="{{URL::asset('js/mainpage.js')}}"></script>
 <script src="{{ URL::asset('js/editQuestion.js') }}" type="text/javascript"></script>
