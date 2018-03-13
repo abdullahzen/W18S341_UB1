5130d0f057c6c8da1819bc27dfa98d5ea7716a79
Abdulla Alhaj Zin <abdullah.hzen@gmail.com>
2018-03-12 21:37:00 -0400

    fox travis

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,9 +14,7 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $response = $this->get('/', array(
-            'uses' => 'ClientController@getHomepage'
-        ));
+        $response = $this->get('/');
 
         $response->assertStatus(200);
     }
