a9db56640a767ed134c63b2d05b6a5ed0c95822e
Abdulla Alhaj Zin <abdullah.hzen@gmail.com>
2018-03-12 22:53:06 -0400

    more temp changes for travis

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="csrf-token" content="">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
