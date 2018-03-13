d23ad3d8c5da1effde3505ee895ec4bbcd3187d7
Abdulla Alhaj Zin <abdullah.hzen@gmail.com>
2018-03-13 19:22:17 -0400

    another test

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -5,17 +5,8 @@
 
 			<div>
 				
-                <?php $categoriesNames = \App\Http\Controllers\ClientControllerHelper::getCategories();
-                        $categories = array();
-                        foreach ($categoriesNames as $key=>$value){
-                            $categories = array_add($categories, $value->category, \App\Http\Controllers\ClientControllerHelper::getPostsByCategoryNameQuery($value->category));
-                        }
-                ?>
-
-                @foreach($categories as $language=>$posts)
-                    @include('pages.category', ['language'=>$language, 'posts'=>$posts])
-
-                @endforeach
+               @include('includes.leftmenu')
+               
             </div>
   </div>
 </body>
