1a1d7fd86d2a9e81888cab007eb7f5190d798698
Nour El Natour <noor_elnatour@hotmail.com>
2018-03-21 23:07:56 -0400

    Implemented all the backend to the front end of the public profile

--- resources/views/pages/publicProfile.blade.php
+++ resources/views/pages/publicProfile.blade.php
@@ -7,15 +7,25 @@
                 <article class="slds-card">
                     <div class="slds-scope" style="padding-top: 15mm; padding-left: 18mm;">
                         <div class="slds-text-body_regular" style="padding-left: 8cm; position: absolute; margin-top: 2.6cm; font-size: x-large;">
-                            user name<br/>
+                            {{ucwords($username)}}<br/>
                             <div class="vl">
-                                <div style="margin-left: 2cm; font-size: large;"> <br/><br/> Ranking: <br/><br/> Questions: <br/><br/> Answers:</div>
+                                <div style="margin-left: 2cm; font-size: large;"> <br/><br/> Rank: {{\App\Http\Controllers\ClientControllerHelper::getRank($username)}}
+                                    <br/><br/> Questions: {{\App\Http\Controllers\ClientControllerHelper::getNumberOfQuestionsForUser($username)}}
+                                    <br/><br/> Answers: {{\App\Http\Controllers\ClientControllerHelper::getNumberOfAnswersForUser($username)}}
+                                </div>
                             </div>
                         </div>
                         <span class="slds-text-align_left slds-avatar slds-avatar_circle slds-avatar_large" style="height: 6cm; width: 6cm;">
                             <img alt="Person name" src="/assets/images/avatar2.jpg" title="User avatar"/>
                         </span>
-                        <div style="margin-top: 2cm; font-size: x-large; font-weight: bold;">User's Questions</div>
+                        <div style="margin-top: 2cm; font-size: x-large; font-weight: bold; margin-left: 7.5cm;">{{ucwords($username)}}'s Questions</div>
+                        <?php $posts = (\App\Http\Controllers\ClientControllerHelper::getAllPostsForUser($username));?>
+                        @foreach ($posts as $data)
+                            @include('includes.postbox', ['data' => $data])
+                        @endforeach
+                        <br>
+                        <br>
+                        <br>
                     </div>
                 </article>
             </div>
@@ -23,3 +33,4 @@
     </div>
 </body>
 <link href="{{URL::asset('css/ourstyle.css')}}" rel="stylesheet" type="text/css">
+@include('includes.modals')
