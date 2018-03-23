d4b1ff912c4cb90d68721f7ef255cb4b9b7a558e
Abdulla Alhaj Zin <abdullah.hzen@gmail.com>
2018-03-23 00:38:53 -0400

    fix to test the new url change maybe it's working now!!!!!

--- tests/Browser/ExampleTest.php
+++ tests/Browser/ExampleTest.php
@@ -24,7 +24,7 @@ class ExampleTest extends DuskTestCase
 
         $this->browse(function (Browser $browser) {
             $browser->visit(new homepage());
-            $browser->waitForText('localhost');
+            $browser->assertSee('Java');
 
         });
     }
