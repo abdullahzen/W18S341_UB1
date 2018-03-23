73ba4f3f40744a3015f9bba5da3d0238e6e4f402
Abdulla Alhaj Zin <abdullah.hzen@gmail.com>
2018-03-23 00:21:09 -0400

    touch

--- tests/Browser/ExampleTest.php
+++ tests/Browser/ExampleTest.php
@@ -24,7 +24,7 @@ class ExampleTest extends DuskTestCase
 
         $this->browse(function (Browser $browser) {
             $browser->visit(new homepage());
-            $browser->assertSee($browser->driver->getCurrentURL());
+            $browser->waitForText('localhost');
 
         });
     }
