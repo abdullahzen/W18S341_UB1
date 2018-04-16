097cb0643895fd6f11fbe9b7edd467f0e5054903
Abdulla Alhaj Zin <abdullah.hzen@gmail.com>
2018-04-16 03:23:23 -0400

    no message

--- tests/Browser/UITest.php
+++ tests/Browser/UITest.php
@@ -22,13 +22,16 @@ class UITest extends DuskTestCase
     }
     public function testHomePage()
     {
+
         $this->checkDbConnection();
 
         $this->prepare();
+        $this->startChromeDriver();
 
         $this->browse(function (Browser $browser) {
             $browser->visit('http://localhost:8080/');
             $browser->waitForText('Java',20);
+            $browser->screenshot('sd');
             $browser->assertSee('Java');
 
         });
